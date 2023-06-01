83edb7f37c1ff564241f36e2a9ce062b596522b3
Rodrigo <rodrigoml2395@gmail.com>
2023-06-01 14:25:03 -0300

    feat: oalindrome iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word=""):
+    if word == "" or len(word) == 0 or type(word) != str:
+        return False
+
+    reversed_word = word[::-1]
+
+    if word.lower() == reversed_word.lower():
+        return True
+
+    return False
