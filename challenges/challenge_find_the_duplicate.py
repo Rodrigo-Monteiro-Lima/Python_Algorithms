e0571d2d584077e3bfe6745aec2c98c1ba359d61
Rodrigo <rodrigoml2395@gmail.com>
2023-06-01 17:52:27 -0300

    refactor: find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,17 +2,16 @@ from challenges.merge_sort import merge_sort
 
 
 def find_duplicate(nums=[]):
-    if not isinstance(nums, list) or len(nums) == 0:
+    try:
+        nums = merge_sort(nums)
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1] and nums[i] >= 0:
+                return nums[i]
+
+        return False
+    except TypeError:
         return False
 
-    for i in range(len(nums)):
-        if not isinstance(nums[i], int) or nums[i] < 0:
-            return False
 
-    nums = merge_sort(nums)
-
-    for i in range(len(nums) - 1):
-        if nums[i] == nums[i + 1]:
-            return nums[i]
-
-    return False
+print(find_duplicate([1]))
