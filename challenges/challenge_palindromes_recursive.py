040aeb58aabca229c895b1c15f0ae0379b5a90e4
Rodrigo <rodrigoml2395@gmail.com>
2023-06-01 14:14:40 -0300

    feat: palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word=None, low_index=None, high_index=None):
+    if word is None or word == "" or low_index is None or high_index is None:
+        return False
+
+    if low_index > high_index:
+        return True
+
+    if word[low_index].lower() != word[high_index].lower():
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
