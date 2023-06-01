a78f3adfb5f22d49439a141cbb2cd760d7afee66
Rodrigo <rodrigoml2395@gmail.com>
2023-06-01 15:21:08 -0300

    test: encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("rodrigo", 3) == "dor_ogir"
+    assert encrypt_message("rodrigo", 4) == "ogi_rdor"
+    assert encrypt_message("rodrigo", 10) == "ogirdor"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("rodrigo", "3")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 3)
