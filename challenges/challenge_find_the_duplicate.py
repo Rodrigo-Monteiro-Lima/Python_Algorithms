f3f3c2b407725e34629b63b52a34c1278fd6b45e
Rodrigo <rodrigoml2395@gmail.com>
2023-06-01 17:21:07 -0300

    fix: find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,16 +1,22 @@
-# from challenges.merge_sort import merge_sort
+from challenges.merge_sort import merge_sort
 
 
-# def find_duplicate(nums=[]):
-#     if type(nums) != list or len(nums) == 0:
-#         return False
+def find_duplicate(nums=[]):
+    if type(nums) != list or len(nums) == 0:
+        return False
 
-#     nums = merge_sort(nums)
+    nums = merge_sort(nums)
 
-#     for num in nums:
-#         if type(num) != int or num < 0:
-#             return False
-#         if nums.count(num) > 1:
-#             return num
+    # for i in range(len(nums)):
+    #      if type(num) != int or num < 0 or i == len(nums) - 1:
+    #         return False
+    #     if nums[i] in nums[i + 1 :]:
+    #         return nums[i]
+    # return False
+    for i, num in enumerate(nums):
+        if type(num) != int or num < 0 or i == len(nums) - 1:
+            return False
+        if nums[i] == nums[i + 1]:
+            return num
 
-#     return False
+    return False
